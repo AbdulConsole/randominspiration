dda8bc031ca820f54ab62f00cc32784c62cb51ca
hzwedt@gmail.com <hzwedt@gmail.com>
2024-01-19 01:10:02 +0100

    updated process

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,39 +1,49 @@
+// Grab DOMs
+let btn = document.querySelector('.container button');
+let quoteText = document.querySelector('.container .quote p');
+let counter = document.querySelector('.inspire');
+let quotes = [];
+let count = 0;
+
+fetchQuote();
+
+// Event Listener
+btn.addEventListener('click', getQuote);
+
+
+//Get Next Quote
+function getQuote() {
+  console.log(quotes);
+  if (count !== 3) {
+    btn.innerHTML = "Get another";
+  }
+  if (count !== 3){
+    quoteText.innerHTML = quotes[getRandom()];
+    //quoteText.style.color = "var(--clr-primary)";
+    count++;
+    counter.textContent = 3 - count;
+    return
+  }
+  quoteText.innerHTML = "Subscribe to get more quotes.<br> contact: abdulconsole@gmail.com</b>";
+  btn.innerHTML = "Subscribe Now"
+  btn.type = "text"
+  btn.style.backgroundColor = "#ddd";
+  btn.style.border = "2px solid #ee6352";
+  counter.style.backgroundColor = 'red';
+}
+// Fetch Quotes
+function fetchQuote() {
+  const jsonFilePath = 'quote.json';
+  // Fetch the JSON file
+  fetch(jsonFilePath)
+  .then(response => response.json())
+  .then(data => {
+    quotes = data;
+  })
+  .catch(error => console.error('Error fetching JSON:', error));
+}
 //Get random index no
 function getRandom(){
     var random = Math.floor(Math.random() * 7) + 1;
     return random;
 }
-
-var count = 0;
-
-//Change Quote
-function quote(){
-    var quotes = [
-    "first of all, nothing here",
-    "The first thing you learn in life is you're a fool. The last thing you learn in life is you're the same fool.  <br> - Ray Bradbury",
-    "When you're curious, you find lots of interesting things to do. - Walt Disney",
-    "Life may be full of pain but that's not an excuse to give up. - Arnold Schwarzenegger",
-    "How you think is as important as as what you think. - Unknown",
-    "You become a worrier by practicing worry. You become free of worry by practicing the opposite. - Norman Vincent Peale",
-    "Life is simply what our feelings do to us. - Honore de Balzac",
-    "I admire those who do good and expect nothing in return. - Roy T. Bennett"
-    ];
-    
-    
-    var para =document.getElementById("para");
-    var btn = document.getElementById("btn");
-
-    if (count !== 3){
-        para.innerHTML = count + quotes[getRandom()];
-        count++;
-        return
-    }
-    para.innerHTML = "Subscribe to get more quotes.<b> contact: hzwedt@gmail.com</b>";
-    btn.innerHTML = "Subscribe Now"
-    btn.type = "text"
-    btn.style.backgroundColor = "#ddd";
-    btn.style.border = "2px solid #ee6352";
-}
-
-
-
